a2bc8058817b5d0b94b5c1294690758ad93dbaf3
Yousof <yousofebneddin@yahoo.com>
2017-07-02 14:42:41 +0200

    update after a while

---
+++ Intro to Vehicle Detection/helpers.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import glob
+
+def plot3d(pixels, colors_rgb,
+        axis_labels=list("RGB"), axis_limits=[(0, 255), (0, 255), (0, 255)]):
+    """Plot pixels in 3D."""
+
+    # Create figure and 3D axes
+    fig = plt.figure(figsize=(8, 8))
+    ax = Axes3D(fig)
+
+    # Set axis limits
+    ax.set_xlim(*axis_limits[0])
+    ax.set_ylim(*axis_limits[1])
+    ax.set_zlim(*axis_limits[2])
+
+    # Set axis labels and sizes
+    ax.tick_params(axis='both', which='major', labelsize=14, pad=8)
+    ax.set_xlabel(axis_labels[0], fontsize=16, labelpad=16)
+    ax.set_ylabel(axis_labels[1], fontsize=16, labelpad=16)
+    ax.set_zlabel(axis_labels[2], fontsize=16, labelpad=16)
+
+    # Plot pixel values with colors given in colors_rgb
+    ax.scatter(
+        pixels[:, :, 0].ravel(),
+        pixels[:, :, 1].ravel(),
+        pixels[:, :, 2].ravel(),
+        c=colors_rgb.reshape((-1, 3)), edgecolors='none')
+
+    return ax  # return Axes3D object for further manipulation
+
+
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel()
+    # Return the feature vector
+    return features
---
+++ Intro to Vehicle Detection/test_helpers.py
@@ -0,0 +1,34 @@
+import matplotlib.image as mpimg
+from helpers import *
+
+##################### Read a color image
+img = cv2.imread("../images/31.png")
+
+# Select a small fraction of pixels to plot by subsampling it
+scale = max(img.shape[0], img.shape[1], 64) / 64  # at most 64 rows and columns
+img_small = cv2.resize(img, (np.int(img.shape[1] / scale), np.int(img.shape[0] / scale)), interpolation=cv2.INTER_NEAREST)
+
+# Convert subsampled image to desired color space(s)
+img_small_RGB = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)  # OpenCV uses BGR, matplotlib likes RGB
+img_small_HSV = cv2.cvtColor(img_small, cv2.COLOR_BGR2HSV)
+img_small_rgb = img_small_RGB / 255.  # scaled to [0, 1], only for plotting
+
+# Plot and show
+plot3d(img_small_RGB, img_small_rgb)
+plot3d(img_small_HSV, img_small_rgb, axis_labels=list("HSV"))
+plt.show()
+
+
+#######################
+# Read in an image
+# You can also read cutout2, 3, 4 etc. to see other examples
+image = mpimg.imread('../images/test_img.jpg')
+
+feature_vec = bin_spatial(image, color_space='RGB', size=(32, 32))
+
+# Plot features
+plt.plot(feature_vec)
+plt.title('Spatially Binned Features')
+plt.show()
+
+##########################
